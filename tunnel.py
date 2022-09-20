bc8916a41af6b97a79f052253101d98d80646ed8
ale <alexander.palma@usach.cl>
2022-09-20 20:19:12 -0300

    minor cleanup and bugfixes

--- tunnel.py
+++ tunnel.py
@@ -3,11 +3,11 @@ from select import select
 from time import strftime
 from traceback import format_exc
 
-LOCAL_HOST = '192.168.0.4'
-REMOTE_HOST = '172.24.169.42'
+LOCAL_HOST = '192.168.32.1'
+REMOTE_HOST = '192.168.0.16'
 
-LOCAL_PORT = 3000
-REMOTE_PORT = 3000
+LOCAL_PORT = 8000
+REMOTE_PORT = 8000
 
 def log(*args):
     print(strftime('[%Y-%m-%d %H:%M:%S]'), *args)
@@ -15,7 +15,6 @@ def log(*args):
 class Port():
     def __init__(self, local_address, remote_address):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind(local_address)
         self.sock.listen()
         self.local_address = local_address
@@ -29,28 +28,28 @@ class Port():
         s1, _ = self.sock.accept()
         s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
+            # FIXME: might block for too long?
             s2.connect(self.remote_address)
         except:
             try:
                 s1.close()
             except:
                 pass
-            return ()
+            return None
 
         local_sock = Tunnel(s1, 'local', self.local_address)
         remote_sock = Tunnel(s2, 'remote', self.remote_address)
         local_sock.peer = remote_sock
         remote_sock.peer = local_sock
-        return (local_sock, remote_sock)
+        return [local_sock, remote_sock]
 
 class Tunnel():
     def __init__(self, sock, half, addr):
         self.sock = sock
         self.half = half
-        self.addr = '{}:{}'.format(*addr)
+        self.addr = addr
         self.buffer = b''
         self.sock.setblocking(0)
-        self.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
         self.done = False
 
     def fileno(self):
@@ -108,29 +107,34 @@ socks = [Port((LOCAL_HOST, LOCAL_PORT), (REMOTE_HOST, REMOTE_PORT))]
 
 log('Ready!')
 while True:
-    rlist, wlist, _ = select(socks, filter(lambda s: s.buffer, socks), (), 2)
+    rlist, wlist, _ = select(socks, filter(lambda s: s.buffer, socks), (), 60)
 
     for s in wlist:
         try:
             result = s.resend()
         except Exception:
-            log('Tunnel failed on', s.peer.sock, '\n' + format_exc())
+            log('Tunnel failed on', s.sock, '\n' + format_exc())
             socks.remove(s)
             continue
 
         if result == -1:
             socks.remove(s)
-            log('Connection shut down successfully ({}, {})'.format(s.addr, s.half))
+            log('Connection shut down successfully',
+                '({}:{}, {})'.format(*s.addr, s.half))
 
         if result > 0:
             socks.remove(s)
-            log('Connection shut down with', result, 'errors on', s.peer.sock,
-                '({}, {})'.format(s.addr, s.half))
+            log('Connection shut down with', result, 'errors',
+                '({}:{}, {})'.format(*s.addr, s.half))
 
     for s in rlist:
-        if s.buffer is None:
-            list(map(socks.append, s.accept()))
-            log('Tunnel opened to', s.remote_address)
+        if s.buffer is None:  # s is Port, not Tunnel
+            new_socks = s.accept()
+            if new_socks:
+                socks += new_socks
+                log('Tunnel opened to {}:{}'.format(*s.remote_address))
+            else:
+                log('Failed to open tunnel to {}:{}'.format(*s.remote_address))
             continue
 
         try:
@@ -142,9 +146,10 @@ while True:
 
         if result == -1:
             socks.remove(s)
-            log('Connection shut down successfully ({}, {})'.format(s.addr, s.half))
+            log('Connection shut down successfully',
+                '({}:{}, {})'.format(*s.addr, s.half))
 
         if result > 0:
             socks.remove(s)
-            log('Connection shut down with', result, 'errors on', s.sock,
-                '({}, {})'.format(s.addr, s.half))
+            log('Connection shut down with', result, 'errors',
+                '({}:{}, {})'.format(*s.addr, s.half))
